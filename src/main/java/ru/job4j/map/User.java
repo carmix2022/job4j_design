a7b3ae97997de18496634ac72fc3ad0f6aa38da3
Илья <ily470@yandex.ru>
2023-03-10 00:05:05 +0700

    7. Переопределить и equals и hashCode [#1002 #398097]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,6 @@
 package ru.job4j.map;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
     private String name;
@@ -11,5 +11,43 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+    public static void main(String[] args) {
+        Calendar bithday = Calendar.getInstance();
+
+        User user1 = new User("Max", 3, bithday);
+        int hashCode1 = user1.hashCode();
+        int hash1 = hashCode1 ^ (hashCode1 >> 16);
+        int bucket1 = hash1 & 15;
+
+        User user2 = new User("Max", 3, bithday);
+        int hashCode2 = user2.hashCode();
+        int hash2 = hashCode2 ^ (hashCode2 >> 16);
+        int bucket2 = hash2 & 15;
+
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        System.out.println(map);
+        System.out.printf("user1 - хэшкод: %s, хэш: %s, бакет: %s %n", hashCode1, hash1, bucket1);
+        System.out.printf("user2 - хэшкод: %s, хэш: %s, бакет: %s", hashCode2, hash2, bucket2);
+        System.out.println(map.get(new User("Max", 3, bithday)));
     }
 }
