b7e3b9ae0993658abd306bf98e3231254a1a015b
Илья <ily470@yandex.ru>
2023-03-17 20:56:15 +0700

    7. Анализ доступности сервера [#859]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -7,18 +7,17 @@ public class Analysis {
     public void unavailable(String source, String target) {
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
         PrintWriter writer = new PrintWriter(target)) {
-            List<String[]> list = reader.lines()
-                    .map(s -> s.split(" "))
-                    .toList();
             boolean server = true;
-            for (String[] s : list) {
-                if (server && ("400".equals(s[0])  || "500".equals(s[0]))) {
+            String s;
+            while ((s = reader.readLine()) != null) {
+                String[] ss = s.split(" ");
+                if (server && ("400".equals(ss[0])  || "500".equals(ss[0]))) {
                     server = false;
-                    writer.printf("%s;", s[1]);
+                    writer.printf("%s;", ss[1]);
                 }
-                if (!server && ("200".equals(s[0])  || "300".equals(s[0]))) {
+                if (!server && ("200".equals(ss[0])  || "300".equals(ss[0]))) {
                     server = true;
-                    writer.printf("%s;%n", s[1]);
+                    writer.printf("%s;%n", ss[1]);
                 }
             }
         } catch (IOException e) {
