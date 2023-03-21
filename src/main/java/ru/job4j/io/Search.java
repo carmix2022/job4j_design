7f6ab287d29daa70ad55f234346c8baada1cd092
Илья <ily470@yandex.ru>
2023-03-21 21:49:46 +0700

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -7,13 +7,30 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        valid(args);
+        Path start = Path.of(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void valid(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Not enough arguments");
+        }
+        Path test = Path.of(args[0]);
+        if (!Files.exists(test)) {
+            throw new IllegalArgumentException(String.format("Not exist %s", test.toAbsolutePath()));
+        }
+        if (!Files.isDirectory(test)) {
+            throw new IllegalArgumentException(String.format("Not directory %s", test.toAbsolutePath()));
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("it's necessary to use a point sign");
+        }
     }
 }
