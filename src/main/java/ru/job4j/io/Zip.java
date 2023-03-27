43cf495ec829a6d3136a07376cc28d099573aba2
Илья <ily470@yandex.ru>
2023-03-27 17:59:28 +0700

    14. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -4,56 +4,55 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
-
-    private final ArgsName argsZip;
-    private final List<File> list;
-
-    public Zip(String[] args) throws IOException {
-        argsZip = ArgsName.of(args);
-        list = Search.search(Path.of(argsZip.get("d")),
-                        p -> !p.toFile().getName().endsWith(argsZip.get("e"))).stream()
-                .map(Path::toFile)
-                .toList();
-    }
-
-    public void packFiles() {
-        File target = new File(argsZip.get("o"));
+    public void packFiles(ArgsName argsName, List<File> list) {
+        File target = new File(argsName.get("o"));
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            int lengthRootPath = Path.of(argsName.get("d")).getNameCount();
             for (File file : list) {
-                packSingleFile(zip, file);
+                String relativePath = file.toPath().subpath(lengthRootPath - 1, file.toPath().getNameCount())
+                        .toString();
+                zip.putNextEntry(new ZipEntry(relativePath));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(file))) {
+                    zip.write(out.readAllBytes());
+                }
+                zip.closeEntry();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void check(String[] args) {
-        if (args.length != 3) {
-            throw new IllegalArgumentException("Incorrect number of arguments");
-        }
-        Path origin = Path.of(args[0].split("=", 2)[1]);
+    public static void check(ArgsName argsName) {
+        Path origin = Path.of(argsName.get("d"));
         if (!Files.exists(origin)) {
             throw new IllegalArgumentException(String.format("Not exist %s", origin.toAbsolutePath()));
         }
-    }
-
-    public void packSingleFile(ZipOutputStream zip, File source) throws IOException {
-        int lengthRootPath = Path.of(argsZip.get("d")).getNameCount();
-        String relativePath = source.toPath().subpath(lengthRootPath - 1, source.toPath().getNameCount()).toString();
-        zip.putNextEntry(new ZipEntry(relativePath));
-        try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-            zip.write(out.readAllBytes());
+        String extension = argsName.get("e");
+        if (!extension.matches("\\.\\w+")) {
+            throw new IllegalArgumentException("Incorrect file extension");
         }
-        zip.closeEntry();
+        String target = argsName.get("o");
+        if (!target.matches(".+\\.zip$")) {
+            throw new IllegalArgumentException("Incorrect target name format");
+        }
     }
 
     public static void main(String[] args) throws IOException {
-        check(args);
-        Zip zip = new Zip(args);
-        zip.packFiles();
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Incorrect number of arguments");
+        }
+        ArgsName argsName = ArgsName.of(args);
+        check(argsName);
+        Predicate<Path> condition = p -> !p.toFile().getName().endsWith(argsName.get("e"));
+        List<File> list = Search.search(Path.of(argsName.get("d")), condition).stream()
+                .map(Path::toFile)
+                .toList();
+        Zip zip = new Zip();
+        zip.packFiles(argsName, list);
     }
 }
